186a53f6a872a9b416b53e055a82571bd4928a2d
Usman <usman.mahmood@brainxtech.com>
2023-05-19 18:30:31 +0500

    Current Location Feature Implemented

--- app/src/main/java/com/example/demo_instaforfood/Fragments/MapFragment/MapFragment.kt
+++ app/src/main/java/com/example/demo_instaforfood/Fragments/MapFragment/MapFragment.kt
@@ -1,12 +1,19 @@
 package com.example.demo_instaforfood.Fragments.MapFragment
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.demo_instaforfood.Models.Place
 import com.example.demo_instaforfood.R
@@ -17,14 +24,21 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.maps.android.ui.IconGenerator
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.GoogleMap
 
 
 class MapFragment : Fragment() {
     lateinit var binding: FragmentMapBinding
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val  LOCATION_PERMISSION_REQUEST_CODE = 101
+    private var locationPermission = MutableLiveData<Boolean>(false)
+
 
     @SuppressLint("PotentialBehaviorOverride")
     private val callback = OnMapReadyCallback { googleMap ->
+
 
         val boundsBuilder = LatLngBounds.Builder()
         val iconGenerator = IconGenerator(requireContext())
@@ -49,6 +63,7 @@ class MapFragment : Fragment() {
             googleMap.addMarker(MarkerOptions().apply {
                 position(latling)
                 icon(BitmapDescriptorFactory.fromBitmap((iconGenerator.makeIcon(count.toString()))))
+
             })
             googleMap.setInfoWindowAdapter(MyInfoWindowAdapter(requireContext()))
 
@@ -66,14 +81,27 @@ class MapFragment : Fragment() {
         }
 
 //        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(),1000,1000,0))
-        googleMap.moveCamera(
-            CameraUpdateFactory.newLatLngBounds(
-                boundsBuilder.build(),
-                500,
-                500,
-                0
-            )
-        )
+//        googleMap.moveCamera(
+//            CameraUpdateFactory.newLatLngBounds(
+//                boundsBuilder.build(),
+//                500,
+//                500,
+//                0
+//            )
+//        )
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        if(checkPermission()) {
+            locationPermission.postValue(true)
+         }
+        else{
+            getPermissions()
+         }
+        locationPermission.observe(requireActivity(),{ pemissionflag ->
+            if(pemissionflag) {
+                getCurrentLocation(googleMap)
+            }
+        })
+    
     }
 
     override fun onCreateView(
@@ -110,6 +138,84 @@ class MapFragment : Fragment() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
+
+    private fun checkPermission():Boolean{
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+    private fun getCurrentLocation(googleMap: GoogleMap) {
+        val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY,100)
+            .setWaitForAccurateLocation(false)
+            .setMinUpdateIntervalMillis(500)
+            .setMaxUpdateDelayMillis(5000)
+            .build()
+
+        var previousMarker: Marker? = null
+
+        // Get the last known location
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+//            fusedLocationProviderClient.lastLocation
+//                .addOnSuccessListener { location: Location? ->
+//                    location?.let {
+//                        // Set the map camera position to the current location
+//                        val latLng = LatLng(location.latitude, location.longitude)
+//                        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+//                        // Add a marker at the current location
+//                        googleMap.addMarker(MarkerOptions().position(latLng))
+//                    }
+//                }
+            fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest,
+                object : com.google.android.gms.location.LocationCallback() {
+                    override fun onLocationResult(locationResult: com.google.android.gms.location.LocationResult) {
+                        locationResult.lastLocation?.let { location ->
+                            val latLng = LatLng(location.latitude, location.longitude)
+                            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+                            previousMarker?.remove()
+                            previousMarker = googleMap.addMarker(MarkerOptions().position(latLng).title("Current Location"))
+
+                        }
+                    }
+                },
+                null
+            )
+        }
+        else{
+            getPermissions()
+        }
+
+    }
+    private fun getPermissions(){
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            LOCATION_PERMISSION_REQUEST_CODE
+        )
+
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if(grantResults.isNotEmpty() && grantResults[0] === PackageManager.PERMISSION_GRANTED){
+
+                    locationPermission.postValue(true)
+                }
+                else {
+                    Toast.makeText(requireContext(), "Please Give Location Persmission", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
+
 
 
 }
